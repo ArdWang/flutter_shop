bf5337f57c2cf89e7df78599df18e4fa088b01d4
Jing Wang <278161009@qq.com>
2020-05-30 18:00:15 +0800

    update rnd

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './pages/index_page.dart';
 
 void main()=>runApp(MyApp());
 
@@ -6,7 +7,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Material(
+        child: MaterialApp(
+           title:'百姓生活+',
+           debugShowCheckedModeBanner:false,
+           theme: ThemeData(
+             primaryColor:Colors.pink,
+           ),
+           home: IndexPage(),
+        ),
+      ),
     );
   }
 }
