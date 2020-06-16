fd3805766ec193263c745adee9b04fd0377801a6
Jing Wang <278161009@qq.com>
2020-06-16 21:17:04 +0800

    update rnd

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../provide/cart.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import './cart_page/cart_item.dart';
+import './cart_page/cart_bottom.dart';
 
 class CartPage extends StatefulWidget {
   @override
@@ -23,13 +25,21 @@ class _CartPageState extends State<CartPage> {
         builder: (context, snapshot){
           if(snapshot.hasData){
             List cartList = Provider.of<CartProvider>(context, listen: true).cartList;
-            return ListView.builder(
-              itemCount: cartList.length,
-              itemBuilder: (context, index){
-                return ListTile(
-                   title: Text(cartList[index].goodsName), 
-                );
-              },
+
+            return Stack(
+              children: <Widget>[
+                ListView.builder(
+                  itemCount: cartList.length,
+                  itemBuilder: (context, index){
+                    return CartItem(cartList[index]);
+                  }
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  child: CartBottom(),
+                ),
+              ],
             );
           }else{
             return Text('正在加载');
@@ -40,7 +50,7 @@ class _CartPageState extends State<CartPage> {
   }
 
   Future<String> _getCartInfo(BuildContext context) async{
-    await Provider.of<CartProvider>(context, listen: true).getCartInfo();
+    await Provider.of<CartProvider>(context, listen: false).getCartInfo();
     return 'end';
   }
 
